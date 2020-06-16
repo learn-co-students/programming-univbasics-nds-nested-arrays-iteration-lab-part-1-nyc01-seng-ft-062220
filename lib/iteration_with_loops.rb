48a8751e23510cfc0813190372b558d38186d26f
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-16 01:26:00 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,15 @@
 
 def find_even_values(src)
-  row = 0 
-  while row < src[row].count do
+  row = 0
+  while row < src.count do
   element = 0
   while element < src[row].count do
-    if src[row][element]/2 == 0 
-      p src[row][element]
-      element += 1
+    if src[row][element]%2 == 0
+      puts src[row][element]
     end
+    element += 1
   end
-  row += 1 
+  row += 1
 
 end
 end
