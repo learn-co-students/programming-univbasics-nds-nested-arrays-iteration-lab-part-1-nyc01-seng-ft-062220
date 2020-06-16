1e6dea7dcd9a45a02654a007b39cbde34c849387
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-16 00:34:49 +0000

    Done.

--- lib/copy.rb
+++ lib/copy.rb
@@ -1,5 +1,9 @@
 
 def find_even_values(src)
+  # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
+  #
+  # Output all even values in each nested array
+  
   row = 0 
   while row < src[row].count do
   element = 0
